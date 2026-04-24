d387a0c51c08ba064fd16e30081e1b02ab613317
0G01D <diogomanueloliveira1@gmail.com>
2026-04-24 13:27:02 +0000

    implementei interfaces de consulta no  portfolio

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.home, name="portfolio_home"),
+    path("cursos/", views.cursos_view, name="portfolio_cursos"),
+    path("ucs/", views.ucs_view, name="portfolio_ucs"),
+    path("professores/", views.professores_view, name="portfolio_professores"),
+    path("alunos/", views.alunos_view, name="portfolio_alunos"),
+    path("competencias/", views.competencias_view, name="portfolio_competencias"),
+    path("formacoes/", views.formacoes_view, name="portfolio_formacoes"),
+    path("tfcs/", views.tfcs_view, name="portfolio_tfcs"),
+    path("projetos/", views.projetos_view, name="portfolio_projetos"),
+    path("tecnologias/", views.tecnologias_view, name="portfolio_tecnologias"),
+    path("inscricoes/", views.inscricoes_view, name="portfolio_inscricoes"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,59 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import (
+    Curso, UC, Professor, Aluno, Competencia,
+    Formacao, TFC, Projeto, Tecnologia, Inscricao
+)
+
 
 def home(request):
-    return HttpResponse("Django está a funcionar")
+    return render(request, "portfolio/home.html")
+
+
+def cursos_view(request):
+    cursos = Curso.objects.prefetch_related("ucs").all()
+    return render(request, "portfolio/cursos.html", {"cursos": cursos})
+
+
+def ucs_view(request):
+    ucs = UC.objects.select_related("curso").prefetch_related("professores", "projetos", "inscricoes").all()
+    return render(request, "portfolio/ucs.html", {"ucs": ucs})
+
+
+def professores_view(request):
+    professores = Professor.objects.select_related("uc", "uc__curso").all()
+    return render(request, "portfolio/professores.html", {"professores": professores})
+
+
+def alunos_view(request):
+    alunos = Aluno.objects.prefetch_related("competencias", "projetos", "inscricao").all()
+    return render(request, "portfolio/alunos.html", {"alunos": alunos})
+
+
+def competencias_view(request):
+    competencias = Competencia.objects.select_related("aluno").prefetch_related("formacoes").all()
+    return render(request, "portfolio/competencias.html", {"competencias": competencias})
+
+
+def formacoes_view(request):
+    formacoes = Formacao.objects.prefetch_related("competencias").all()
+    return render(request, "portfolio/formacoes.html", {"formacoes": formacoes})
+
+
+def tfcs_view(request):
+    tfcs = TFC.objects.prefetch_related("tecnologias").all()
+    return render(request, "portfolio/tfcs.html", {"tfcs": tfcs})
+
+
+def projetos_view(request):
+    projetos = Projeto.objects.select_related("uc", "uc__curso").prefetch_related("alunos", "tecnologias").all()
+    return render(request, "portfolio/projetos.html", {"projetos": projetos})
+
+
+def tecnologias_view(request):
+    tecnologias = Tecnologia.objects.select_related("projeto", "tfc").all()
+    return render(request, "portfolio/tecnologias.html", {"tecnologias": tecnologias})
+
+
+def inscricoes_view(request):
+    inscricoes = Inscricao.objects.select_related("aluno", "uc", "uc__curso").all()
+    return render(request, "portfolio/inscricoes.html", {"inscricoes": inscricoes})
